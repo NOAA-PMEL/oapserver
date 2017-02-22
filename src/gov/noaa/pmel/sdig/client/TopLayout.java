034659dcb48abe21fd6e6f90700d713b1b0eab69
Eugene Burger <eugene.burger@noaa.gov>
2017-02-21 20:09:11 -0800

    Daily commit

--- src/gov/noaa/pmel/sdig/client/TopLayout.java
+++ src/gov/noaa/pmel/sdig/client/TopLayout.java
@@ -5,6 +5,7 @@ package gov.noaa.pmel.sdig.client;
 
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import gov.noaa.pmel.sdig.client.panels.AbstractPanel;
 import gov.noaa.pmel.sdig.client.panels.InvestigatorPanel;
 import gov.noaa.pmel.sdig.client.panels.SubmitterPanel;
 
@@ -18,9 +19,11 @@ public class TopLayout extends VerticalPanel {
 		InvestigatorPanel panel1 = new InvestigatorPanel();
 		InvestigatorPanel panel2 = new InvestigatorPanel();
 		SubmitterPanel panel3 = new SubmitterPanel();
+		AbstractPanel panel4 = new AbstractPanel();
 		this.add(panel1);
 		this.add(panel2);
 		this.add(panel3);
+		this.add(panel4);
 	}
 
 
